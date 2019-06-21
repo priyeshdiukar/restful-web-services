e729be0f2331dbbf568abb3cc742a74ed86dafef
priyesh.diukar <priyesh.diukar@siemens.com>
2019-06-21 18:25:17 +0530

    Add locale demo

--- src/main/java/com/udemy/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/udemy/webservices/restfulwebservices/user/UserResource.java
@@ -2,21 +2,31 @@ package com.udemy.webservices.restfulwebservices.user;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
+
+	@Autowired
+	private MessageSource messageSource;
 
 	@Autowired
 	private UserDaoService service;
@@ -27,12 +37,15 @@ public class UserResource {
 	}
 
 	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable int id) {
+	public Resource<User> retrieveUser(@PathVariable int id) {
 		User user = service.findOne(id);
 		if (user == null) {
 			throw new UserNotFoundException("id-" + id);
 		}
-		return user;
+		Resource<User> resource = new Resource<User>(user);
+		ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		resource.add(linkTo.withRel("all-users"));
+		return resource;
 	}
 
 	@PostMapping("/users")
@@ -49,6 +62,11 @@ public class UserResource {
 		if (user == null) {
 			throw new UserNotFoundException("id-" + id);
 		}
+	}
+
+	@GetMapping("/users/messages")
+	public String messageInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+		return messageSource.getMessage("good.morning.message", null, locale);
 	}
 
 }
