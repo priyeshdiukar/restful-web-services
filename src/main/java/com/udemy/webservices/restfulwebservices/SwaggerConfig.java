d52e10db11c2d4d4fc99c478df08543dd43c489b
priyesh.diukar <priyesh.diukar@siemens.com>
2019-09-19 10:51:00 +0530

    Update swagger docs with custom details. Enhance swagger docs with validation and custom notes.

--- src/main/java/com/udemy/webservices/restfulwebservices/SwaggerConfig.java
+++ src/main/java/com/udemy/webservices/restfulwebservices/SwaggerConfig.java
@@ -1,8 +1,14 @@
 package com.udemy.webservices.restfulwebservices;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,9 +17,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+	public static final Contact DEFAULT_CONTACT = new Contact("Priyesh Diukar", "website.com", "abc.xyz@google.com");
+	public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Test Api Documentation Title", "Test Api Documentation",
+			"1.0", "urn:tos", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+	
+	private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(Arrays.asList("application/json", "application/xml"));
+
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2);
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(DEFAULT_API_INFO)
+				.produces(DEFAULT_PRODUCES_AND_CONSUMES);
 	}
-	
+
 }
--- src/main/java/com/udemy/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/udemy/webservices/restfulwebservices/user/User.java
@@ -5,14 +5,20 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "Details about user")
 public class User {
 
 	private Integer id;
 	
-	@Size(min=2, message="Name should have atleast 2 characters.")
+	@Size(min=2, message="Name should have atleast 2 characters")
+	@ApiModelProperty(notes="Name should have atleast 2 characters")
 	private String name;
 
 	@Past
+	@ApiModelProperty(notes="Birthdate should be in past")
 	private Date birthDate;
 	
 	protected User() {
